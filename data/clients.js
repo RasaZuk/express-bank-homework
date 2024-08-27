e1e38024077240ab72e8dfecef8306f01300d6b8
Rasa <rasa.zukauskaite@gmail.com>
2024-08-27 18:03:38 +0300

    first try

---
+++ data/clients.js
@@ -0,0 +1,24 @@
+export const clients = {
+    Jonas: {
+        "name": 'Jonas',
+        "surname": 'Jonaitis',
+        "dateOfBirth": '2000-01-01',
+        "balance": '100',
+    },
+    Ona: {
+        "name": 'Ona',
+        "surname": 'Onaityte',
+        "dateOfBirth": '2001-07-07',
+        "balance": '150',
+    },
+    Antanas: {
+        "name": 'Antanas',
+        "surname": 'Antanaitis',
+        "dateOfBirth": '2003-03-03',
+        "balance": '200',
+    },
+
+};
+
+
+
--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import { bankRouter } from './bankRouter.js';
 
 const app = express();
 const port = 5030;
+
+// for parsing application/json
+app.use(express.json({
+    type: 'application/json',
+}));
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/bank', bankRouter);
+
+
+app.get('*', (req, res) => {
+    return res.send('Klaida 404. Puslapis nerastas.');
+});
+
 
 app.listen(port, () => {
     console.log(`Serveris pasileido: http://localhost:${port}`);
